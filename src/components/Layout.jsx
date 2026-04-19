e0c30851855305dc28cfefdb19c1e7474a4e3457
Hans Eyma <Hans.Eyma@outlook.com>
2026-04-19 19:26:53 -0400

    feat: wire Toast and Ministries providers; render ToastContainer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,7 @@
+// src/components/Layout.jsx
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
+import ToastContainer from './Toast'
 
 export default function Layout() {
   return (
@@ -8,6 +10,7 @@ export default function Layout() {
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <Outlet />
       </main>
+      <ToastContainer />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './contexts/AuthContext'
+import { ToastProvider } from './contexts/ToastContext'
+import { MinistriesProvider } from './contexts/MinistriesContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ToastProvider>
+          <MinistriesProvider>
+            <App />
+          </MinistriesProvider>
+        </ToastProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
